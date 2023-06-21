ae2ff35adcf180a86502f6fce050c9768a51f993
Zhiyuan Liu <zhiy@cs.unc.edu>
2023-06-20 21:25:10 -0700

    feature analysis with PNS

--- shanapy/models/stats/feature_engineering.py
+++ shanapy/models/stats/feature_engineering.py
@@ -1,5 +1,6 @@
 import vtk
 import numpy as np
+from .principal_nested_spheres import PNS
 class SrepFeatures():
     def __init__():
         pass
@@ -30,6 +31,46 @@ class SrepFeatures():
             dirs[:, i] = direction
 
         return skeletal_pts, dirs, radii
+
+    @staticmethod
+    def euclideanized_srep_features(srep_file_name):
+        """
+        Euclideanize spokes features and skeletal points given a srep file
+        Output tuples: skeletal_pts (3 x n), dirs (3 x n), radii (1 x n), where n denotes 
+                        the number of skeletal points
+        """
+        skeletal_pts, dirs, radii = SrepFeatures.default_srep_features(srep_file_name)
+
+        # Euclideanize angle
+        pns_model = PNS(dirs, itype=9)
+        pns_model.fit()
+        euc_dirs, PNS_coords_dirs = pns_model.output
+
+        # Euclideanize skeletal points 
+        pdms_xs = skeletal_pts[:, ::3]
+        pdms_ys = skeletal_pts[:, 1::3]
+        pdms_zs = skeletal_pts[:, 2::3]
+
+        centered_xs = pdms_xs - np.mean(pdms_xs, axis=1)[:, None]
+        centered_ys = pdms_ys - np.mean(pdms_ys, axis=1)[:, None]
+        centered_zs = pdms_zs - np.mean(pdms_zs, axis=1)[:, None]
+        var = np.sqrt(centered_xs ** 2 + centered_ys ** 2 + centered_zs ** 2)
+
+        scaled_xs = centered_xs / var
+        scaled_ys = centered_ys / var
+        scaled_zs = centered_zs / var
+
+        scaled_pdms = np.concatenate((scaled_xs, scaled_ys, scaled_zs), axis=1)
+        pns_model_pdm = PNS(scaled_pdms, itype=9)
+        pns_model_pdm.fit()
+        euc_pdm, PNS_coords_pdmss = pns_model_pdm.output
+
+        # Commensurate radii
+        log_radii = np.log(radii)
+        geo_mean = np.exp(np.mean(log_radii, dim=-1))
+        radii_comm = (log_radii - geo_mean)
+
+        return euc_pdm, euc_dirs, radii_comm
 
     
 
--- shanapy/test/test_classification.py
+++ shanapy/test/test_classification.py
@@ -3,18 +3,19 @@ import numpy as np
 from shanapy.models.stats import Classification, SrepFeatures
 
 # distribution of initial s-reps features
-root = 'hippocampi/'
+root = '/Users/zhiy/playpen/hippocampi/nick_hipp/'
 os.chdir(root)
 class_names = ["pos", "neg"]
 labels = np.array([1] * 34 + [0] * 143)
 all_feats = []
 for class_name in class_names:
-    directory = class_name + "_initial_sreps"
+    directory = class_name + "_sreps"
     feats_a_group = []
     for file_name in os.listdir(directory):
         if file_name.split('.')[-1] != 'vtk': continue
         
-        skeletal_pts, dirs, radii = SrepFeatures.default_srep_features(root + directory + "/" + file_name)
+        # skeletal_pts, dirs, radii = SrepFeatures.default_srep_features(root + directory + "/" + file_name)
+        skeletal_pts, dirs, radii = SrepFeatures.euclideanized_srep_features(root + directory + "/" + file_name)
         feats = np.concatenate((skeletal_pts, dirs, radii)).flatten()
         feats_a_group.append(feats)
     feats_a_group = np.array(feats_a_group).T # d x n
